35580691d2042c42389f5c72786d0f3c0e4b6e78
Mathias Hillmann <mathias.hillmann@gmail.com>
2023-08-21 20:19:04 -0300

    paste fix

--- src/directives/username-input.directive.ts
+++ src/directives/username-input.directive.ts
@@ -1,26 +1,23 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
+import { NgControl } from '@angular/forms';
 
 @Directive({
   selector: '[UsernameInput]',
 })
 export class UsernameInputDirective {
-  constructor(private el: ElementRef) {}
+  constructor(private el: ElementRef, private control: NgControl) {}
 
   @HostListener('keypress', ['$event']) onKeyPress(event: KeyboardEvent) {
     return new RegExp('^[a-zA-Z0-9\\- ]*$').test(event.key);
   }
 
   @HostListener('paste', ['$event']) blockPaste(event: ClipboardEvent) {
-    this.validateFields(event);
-  }
-
-  validateFields(event: ClipboardEvent) {
     if (event.clipboardData) {
       event.preventDefault();
       const pasteData = event.clipboardData
         .getData('text/plain')
         .replace(/[^a-zA-Z0-9\- ]/g, '');
-      this.el.nativeElement.value = pasteData;
+      this.control.control!.setValue(pasteData);
     }
   }
 }
