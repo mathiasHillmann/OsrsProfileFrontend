be257dc6464ccba81fdccf64764d63f36a1898df
Mathias Hillmann <mathias.hillmann@gmail.com>
2023-11-11 10:45:17 -0300

    fixes

--- src/app/player/combat-tasks-panel/combat-task-group/combat-task-list/combat-task-list.component.ts
+++ src/app/player/combat-tasks-panel/combat-task-group/combat-task-list/combat-task-list.component.ts
@@ -1,10 +1,13 @@
 import { Component, Inject } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { CombatTask } from 'src/interfaces/player-data';
 
 export interface CombatTasksList {
   title: string;
   tasks: CombatTask[];
+  bossList?: string[];
+  boss?: string;
 }
 
 @Component({
@@ -13,10 +16,47 @@ export interface CombatTasksList {
   styleUrls: ['./combat-task-list.component.scss'],
 })
 export class CombatTasksListComponent {
+  bossFilter: FormControl<string | null> = new FormControl<string | null>(
+    'all'
+  );
+  statusFilter: FormControl<string | null> = new FormControl<string | null>(
+    'all'
+  );
+  tierFilter: FormControl<string | null> = new FormControl<string | null>(
+    'all'
+  );
+  bossList: string[] = [];
+  tierList: string[] = [];
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: CombatTasksList,
     public dialogRef: MatDialogRef<CombatTasksListComponent>
-  ) {}
+  ) {
+    this.bossList = [...new Set(this.data.tasks.map((task) => task.monster))];
+    this.tierList = [...new Set(this.data.tasks.map((task) => task.tier))];
+  }
+
+  getFilteredList() {
+    let list: CombatTask[] = this.data.tasks;
+
+    if (this.bossFilter.value && this.bossFilter.value !== 'all') {
+      list = list.filter((task) => task.monster === this.bossFilter.value);
+    }
+
+    if (this.statusFilter.value && this.statusFilter.value !== 'all') {
+      list = list.filter(
+        (task) =>
+          task.completed ===
+          (this.statusFilter.value === 'completed' ? true : false || null)
+      );
+    }
+
+    if (this.tierFilter.value && this.tierFilter.value !== 'all') {
+      list = list.filter((task) => task.tier === this.tierFilter.value);
+    }
+
+    return list;
+  }
 
   getTaskTitleColor(task: CombatTask) {
     if (task.completed) {
